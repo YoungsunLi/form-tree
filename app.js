c842cc4c12b6b24a19e1b05e333c29e6f255dbf6
Youngsun <lsun@live.cn>
2019-06-29 15:31:55 +0800

    递归所有节点 记录深度

--- app.js
+++ app.js
@@ -2,7 +2,27 @@ import treeData from "./tree_data.js";
 
 class App {
     run() {
-        console.log('run');
+        this.findChild(treeData);
+    };
+
+    findChild(tree, level = 0) {
+        level++; // 进入一层递归相当于进入下一级
+        tree.forEach(child => {
+            console.log(child.name);
+
+            if (child.children.length === 0) { // 到达当前树杈的尽头
+
+            } else {
+                this.findChild(child.children, level); // 仍有子级继续递归
+            }
+
+        });
+
+        level--; // 退出一层递归相当于回到父级
+
+        if (level === 0) { // 表示递归结束
+            console.log("done");
+        }
     }
 }
 
