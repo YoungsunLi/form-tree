70c2200a771e143fc3e68541ea561ebb14a36036
Youngsun <lsun@live.cn>
2019-06-30 01:38:47 +0800

    通配约定Array类型为子级  判断逻辑调整

--- app.js
+++ app.js
@@ -2,30 +2,34 @@ import treeData from "./tree_data.js";
 
 class App {
     run() {
-        this.findChild(treeData);
+        let nodeList = this.findChild(treeData) || [];
+        nodeList.forEach(node => {
+            console.log(node);
+        })
     };
 
-    findChild(tree, level = 0, nodePath = {}) {
+    findChild(tree = [], level = 0, nodePath = {}, nodeList = []) {
         level++; // 进入一层递归相当于进入下一级
         tree.forEach(child => {
             Object.keys(child).forEach(key => {
-                if (key !== 'children') { // 适合子级字段用'children'的多叉树数据
+                let nodePathNew = Object.assign({}, nodePath);
+
+                if (child[key] instanceof Array) { // 通配约定Array类型为子级(暂不支持连空Array都没有的数据结构)
+                    if (child[key].length === 0) { // 到达当前树径的尽头
+                        nodeList.push(nodePathNew);
+                    } else { // 仍有子级继续递归
+                        this.findChild(child[key], level, nodePathNew, nodeList);
+                    }
+                } else { // 追加
                     nodePath[key + level] = child[key];
                 }
             });
-
-            if (child.children.length === 0) { // 到达当前树径的尽头
-                console.log(nodePath);
-            } else {
-                let nodePathNew = Object.assign({}, nodePath);
-                this.findChild(child.children, level, nodePathNew); // 仍有子级继续递归
-            }
         });
 
         level--; // 退出一层递归相当于回到父级
 
         if (level === 0) { // 表示递归结束
-            console.log("done");
+            return nodeList;
         }
     }
 }
